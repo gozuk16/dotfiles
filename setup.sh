04b72824b1e59d71e1d7107de0a53654034f5a03
GOZU Kenichiro <gozuk16@gmail.com>
2026-02-12 18:34:40 +0900

    .configのセットアップを追加

--- setup.sh
+++ setup.sh
@@ -4,9 +4,26 @@ SOURCE_DIR=`pwd`
 DESTINATION_DIR=~
 DOTFILES=".zshrc-gozu .gitconfig .gitignore_global .vimrc .vim"
 
+CONFIG_SOURCE_DIR=$SOURCE_DIR/.config
+CONFIG_DESTINATION_DIR=~/.config
+CONFIG_FILES="nvim ghostty"
+
 for F in $DOTFILES; do
 	SF=$SOURCE_DIR/$F
 	DF=$DESTINATION_DIR/$F
+	if [ -f $SF ]; then
+		if [ -f $DF ]; then
+			echo "exist $DF"
+		else
+			echo "ln -s $SF $DF"
+			ln -s $SF $DF
+		fi
+	fi
+done
+
+for F in $CONFIG_FILES; do
+	SF=$CONFIG_SOURCE_DIR/$F
+	DF=$CONfiG_DESTINATION_DIR/$F
 	if [ -f $SF ]; then
 		if [ -f $DF ]; then
 			echo "exist $DF"
